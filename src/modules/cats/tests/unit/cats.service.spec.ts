fb8be0ede62a87045b4d1fedc31018c042c3d525
Walid Newaz <mdwalid@gmail.com>
2023-06-20 19:21:38 -0600

    Added mock logging service

--- src/modules/cats/tests/unit/cats.service.spec.ts
+++ src/modules/cats/tests/unit/cats.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsService } from '../../cats.service';
+import { LoggingService } from '../../../logging/logging.service';
+
+const mockLoggingService = {
+  log: jest.fn(),
+};
 
 describe('CatsService', () => {
   let service: CatsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CatsService],
+      providers: [
+        CatsService,
+        {
+          provide: LoggingService,
+          useValue: mockLoggingService,
+        },
+      ],
     }).compile();
 
     service = module.get<CatsService>(CatsService);
